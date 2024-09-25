949488925ee308fbc3e5414123ee91705ce114d2
accel invertsys <accelerators@invertsys.com>
2024-09-24 23:22:50 -0400

    invertsys lza code

---
+++ appconfig/panw-userdata.sh
@@ -0,0 +1,8 @@
+type=dhcp-client
+op-command-modes=jumbo-frame,mgmt-interface-swap
+plugin-op-commands=aws-gwlb-inspect:enable
+op-cmd-dpdk-pkt-io=on
+dhcp-send-hostname=yes
+dhcp-send-client-id=yes
+dhcp-accept-server-hostname=yes
+dhcp-accept-server-domain=yes
